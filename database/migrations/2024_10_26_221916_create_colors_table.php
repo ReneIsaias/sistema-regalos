8cb8259cb5f1fc1fe37b8aa407bb3d3437ddbc5f
ReneIsaias <rene030498@gmail.com>
2024-10-26 16:43:20 -0600

    Add basic tables and update dependencies

--- database/migrations/2024_10_26_221916_create_colors_table.php
+++ database/migrations/2024_10_26_221916_create_colors_table.php
@@ -11,17 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('colors', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('lastname')->nullable();
-            $table->string('username')->unique();
-            $table->string('phone');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('color')->unique();
+            $table->string('slug');
+            $table->string('description')->nullable();
             $table->boolean('status')->default(true);
-            $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
         });
@@ -32,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('colors');
     }
 };
